8b4a7ff06adba0b19f5fd79724027f25944bef23
Schwimpy <simon.winther@htmail.com>
2017-11-24 22:07:23 +0100

    Added better functionality to the avatar swapppingz

--- app/src/main/java/se/iths/apostolidis/quickmaths/AvatarActivity.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/AvatarActivity.java
@@ -13,9 +13,9 @@ public class AvatarActivity extends AppCompatActivity {
     private TypedArray facePictures;
     private int faceCount = 0;
 
-    private ImageView eyes;
-    private TypedArray eyesPictures;
-    private int eyesCount = 0;
+    private ImageView eyeRight;
+    private TypedArray eyeRightPictures;
+    private int eyeRightCount = 0;
 
     private ImageView mouth;
     private TypedArray mouthPictures;
@@ -25,9 +25,9 @@ public class AvatarActivity extends AppCompatActivity {
     private TypedArray hairPictures;
     private int hairCount = 0;
 
-    private ImageView mouthAccesory;
-    private TypedArray mouthAccesoryPictures;
-    private int mouthAccesoryCount = 0;
+    private ImageView eyeLeft;
+    private TypedArray eyeLeftPictures;
+    private int eyeLeftCount = 0;
 
     private ImageView nose;
     private TypedArray nosePictures;
@@ -43,8 +43,8 @@ public class AvatarActivity extends AppCompatActivity {
         facePictures = getResources().obtainTypedArray(R.array.face);
         face = findViewById(R.id.imageViewFace);
 
-        eyesPictures = getResources().obtainTypedArray(R.array.eyes);
-        eyes = findViewById(R.id.imageViewEyes);
+        eyeRightPictures = getResources().obtainTypedArray(R.array.righteye);
+        eyeRight = findViewById(R.id.imageViewRightEye);
 
         mouthPictures = getResources().obtainTypedArray(R.array.mouth);
         mouth = findViewById(R.id.imageViewMouth);
@@ -52,57 +52,96 @@ public class AvatarActivity extends AppCompatActivity {
         hairPictures = getResources().obtainTypedArray(R.array.hair);
         hair = findViewById(R.id.imageViewHair);
 
-        mouthAccesoryPictures = getResources().obtainTypedArray(R.array.mouthaccesory);
-        mouthAccesory = findViewById(R.id.imageViewMouthAccesory);
+        eyeLeftPictures = getResources().obtainTypedArray(R.array.lefteye);
+        eyeLeft = findViewById(R.id.imageViewLeftEye);
 
-        nosePictures = getResources().obtainTypedArray(R.array.scar);
+        nosePictures = getResources().obtainTypedArray(R.array.nose);
         nose = findViewById(R.id.imageViewNose);
     }
 
-    public void faceButton (View view) {
+    public void faceButtonRight (View view) {
         faceCount++;
         if (faceCount > facePictures.length() - 1)
             faceCount = 0;
         face.setImageResource(facePictures.getResourceId(faceCount, 0));
             }
 
+    public void faceButtonLeft (View view) {
+        faceCount--;
+        if (faceCount < 0)
+            faceCount = facePictures.length()-1;
+        face.setImageResource(facePictures.getResourceId(faceCount, 0));
+    }
 
 
-    public void mouthButton (View view) {
+    public void mouthButtonRight (View view) {
         mouthCount++;
         if( mouthCount > mouthPictures.length()-1)
             mouthCount = 0;
         mouth.setImageResource(mouthPictures.getResourceId(mouthCount, 0));
         }
 
+    public void mouthButtonLeft (View view) {
+        mouthCount--;
+        if( mouthCount < 0)
+            mouthCount = mouthPictures.length()-1;
+        mouth.setImageResource(mouthPictures.getResourceId(mouthCount, 0));
+    }
 
-    public void eyeRighthButton (View view) {
-        eyesCount++;
-        if (eyesCount > eyesPictures.length() - 1)
-            eyesCount = 0;
-        eyes.setImageResource(eyesPictures.getResourceId(eyesCount, 0));
+    public void rightEyeButtonRight (View view) {
+        eyeRightCount++;
+        if (eyeRightCount > eyeRightPictures.length() - 1)
+            eyeRightCount = 0;
+        eyeRight.setImageResource(eyeRightPictures.getResourceId(eyeRightCount, 0));
             }
 
-    public void eyeLeftButton (View view) {
-        mouthAccesoryCount++;
-        if( mouthAccesoryCount > mouthAccesoryPictures.length()-1)
-            mouthAccesoryCount = 0;
-        mouthAccesory.setImageResource(mouthAccesoryPictures.getResourceId(mouthAccesoryCount, 0));
+    public void rightEyeButtonLeft (View view) {
+        eyeRightCount--;
+        if (eyeRightCount < 0)
+            eyeRightCount = eyeRightPictures.length()-1;
+        eyeRight.setImageResource(eyeRightPictures.getResourceId(eyeRightCount, 0));
+    }
+
+    public void leftEyeButtonRight (View view) {
+        eyeLeftCount++;
+        if( eyeLeftCount > eyeLeftPictures.length()-1)
+            eyeLeftCount = 0;
+        eyeLeft.setImageResource(eyeLeftPictures.getResourceId(eyeLeftCount, 0));
             }
 
-    public void hairButton (View view) {
+    public void leftEyeButtonLeft (View view) {
+        eyeLeftCount--;
+        if( eyeLeftCount < 0)
+            eyeLeftCount = eyeLeftPictures.length()-1;
+        eyeLeft.setImageResource(eyeLeftPictures.getResourceId(eyeLeftCount, 0));
+    }
+
+    public void hairButtonRight (View view) {
         hairCount++;
         if( hairCount > hairPictures.length()-1)
             hairCount = 0;
         hair.setImageResource(hairPictures.getResourceId(hairCount, 0));
             }
 
-    public void noseButton (View view) {
+    public void hairButtonLeft (View view) {
+        hairCount--;
+        if( hairCount < 0)
+            hairCount = hairPictures.length()-1;
+        hair.setImageResource(hairPictures.getResourceId(hairCount, 0));
+    }
+    public void noseButtonRight (View view) {
         noseCount++;
         if( noseCount > nosePictures.length()-1)
             noseCount = 0;
         nose.setImageResource(nosePictures.getResourceId(noseCount, 0));
             }
+
+    public void noseButtonLeft (View view) {
+        noseCount--;
+        if( noseCount < 0)
+            noseCount = nosePictures.length()-1;
+        nose.setImageResource(nosePictures.getResourceId(noseCount, 0));
+    }
 
     public void backButton (View view) {
         Intent intent = new Intent(this, AboutActivity.class);
