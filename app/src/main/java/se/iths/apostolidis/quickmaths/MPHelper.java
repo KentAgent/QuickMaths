17473b28f4b59d3d431a65c3683096fcb10358ab
kakashidota <robin.kamo@live.se>
2017-11-27 11:25:05 +0100

    Branching out

---
+++ app/src/main/java/se/iths/apostolidis/quickmaths/MPHelper.java
@@ -0,0 +1,8 @@
+package se.iths.apostolidis.quickmaths;
+
+/**
+ * Created by Kakashi on 26/11/17.
+ */
+
+public class MPHelper {
+}
--- app/src/main/java/se/iths/apostolidis/quickmaths/MultiplayerActivity.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/MultiplayerActivity.java
@@ -1,32 +1,26 @@
 package se.iths.apostolidis.quickmaths;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.games.PlayersClient;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.TurnBasedMultiplayerClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MultiplayerActivity extends AppCompatActivity {
 
-    GoogleSignInClient mGoogleSignInClient;
-    GoogleSignInAccount googleUser;
-    private static final int RC_SIGN_IN = 234;
-    private static final String TAG = "Kakashidota";
-    GoogleSignInOptions gso;
     TextView testUsername;
     FirebaseAuth mAuth;
     FirebaseUser bror;
-    PlayersClient playersClient;
+    Context context;
+    Button startMatch;
+    private GoogleSignInAccount user;
 
 
     @Override
@@ -34,51 +28,22 @@ public class MultiplayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplayer);
         testUsername = findViewById(R.id.testUser);
-
+        context = getApplicationContext();
+        startMatch = findViewById(R.id.ButtonStartMatch);
         mAuth = FirebaseAuth.getInstance();
         bror = mAuth.getCurrentUser();
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
 
-         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        user = GoogleSignIn.getLastSignedInAccount(this);
+
+        TurnBasedMultiplayerClient turnBasedMultiplayerClient = Games.getTurnBasedMultiplayerClient(this, user);
 
 
-      //  testUsername.setText(googleUser.getEmail().toString());
+        testUsername.setText(user.getEmail());
 
 
 
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        //if the requestCode is the Google Sign In code that we defined at starting
-        if (requestCode == RC_SIGN_IN) {
-
-            //Getting the GoogleSignIn Task
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                //Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-
-                //authenticating with firebase
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-
-
-    }
-
-
-    public void test(){
-       // Intent myintent = g
-    }
 }
+
+
