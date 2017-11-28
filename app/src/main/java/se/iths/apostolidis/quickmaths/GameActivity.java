1ae83c9345288d9d42302f80eb97321673769b08
WilliamLennartsson <31884043+WilliamLennartsson@users.noreply.github.com>
2017-11-28 18:15:37 +0100

    Canvas update

--- app/src/main/java/se/iths/apostolidis/quickmaths/GameActivity.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/GameActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
@@ -35,10 +36,7 @@ public class
 GameActivity extends AppCompatActivity {
 
     MPhotoView map;
-    private Point point = new Point();
-    private static int gridSize = 35;
-    private Point[] coordinatesX = new Point[gridSize];
-    private Point[] coordinatesY = new Point[gridSize];
+    private static int gridSize = 37;
     private int maxX;
     private int maxY;
     // private View view = new View(this);
@@ -47,7 +45,7 @@ GameActivity extends AppCompatActivity {
 
     SurfaceView surfaceView;
 
-
+    private Point[] assetCoordinates = new Point[gridSize];
     private Canvas canvas;
     private Drawable d;
     private SurfaceHolder surfaceHolder;
@@ -65,42 +63,118 @@ GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         map = findViewById(R.id.photo_view);
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.face1);
-        map.setScaleType(PhotoView.ScaleType.FIT_XY);
-        map.setImageResource(R.mipmap.gamemap);
+        //map.setImageResource(R.mipmap.gamemap);
+        //map.setImageBitmap(setMap());
+
+
+        assetCoordinates = new Point[gridSize];
+        setAssetPosList(assetCoordinates);
+
+        map.setImageBitmap(setMap(assetCoordinates));
     }
 
-
-    public void getCoordinateGrid (){
-        Display display = getWindowManager().getDefaultDisplay();
-        Point displaySize = new Point();
-        display.getSize(displaySize);
-        Log.d("Wille", displaySize.toString());
-
-        maxY = displaySize.y;
-        maxX = displaySize.x;
-
-        Log.e("Wille", displaySize.toString());
+    public Bitmap setMap (Point[] assetCoordinates){
 
 
-        final float spaceX = (float) (maxX * 0.05);
-        final float spaceY = (float) (maxY * 0.05);
+        Bitmap backgroundImage = BitmapFactory.decodeResource(getResources(), R.mipmap.gamemap);
+        Bitmap mergedCoodinateAssets = backgroundImage;
 
-        double increment = 0;
-        for (int i = 0; i < gridSize; i++) {
-            Point point = new Point();
-            point.set(0, (int) (increment += spaceY));
-            coordinatesY[i] = point;
-            Log.d("Wille", "Y: " + coordinatesY[i].toString());
+        Bitmap previousImage;
+        Bitmap scaledCoordinateAsset = backgroundImage;
+        for (int i = 0; i < assetCoordinates.length; i++){
+
+            if (i == 0) {
+                previousImage = backgroundImage;
+            } else {
+                previousImage = mergedCoodinateAssets;
+            }
+            Bitmap coordinateAsset = BitmapFactory.decodeResource(getResources(), R.drawable.gameboardassets_maths);
+            scaledCoordinateAsset = Bitmap.createScaledBitmap(coordinateAsset, 50, 50, false);
+            Log.d("Wille", "X value: " + assetCoordinates[i].x);
+
+            mergedCoodinateAssets = createSingleImageFromMultipleImages(previousImage, scaledCoordinateAsset, 0, 0, assetCoordinates[i].x + 20, assetCoordinates[i].y + 20);
         }
-        increment = 0;
-        for (int i = 0; i < gridSize; i++){
-            Point point = new Point();
-            point.set((int) (increment += spaceX), 0);
-            coordinatesX[i] = point;
-            Log.d("Wille", "X: " + coordinatesX[i].toString());
-        }
-        //map.setCoordinatesX(coordinatesX);
-        //map.setCoordinatesY(coordinatesY);
+
+
+        Log.d("Wille", "Bigmerge: " + mergedCoodinateAssets.toString());
+        return mergedCoodinateAssets;
+
+    }
+
+    private Bitmap createSingleImageFromMultipleImages(Bitmap firstImage, Bitmap secondImage, int firstImageX, int firstImageY, int secondImageX, int secondImageY){
+
+        Bitmap result = Bitmap.createBitmap(firstImage.getWidth(), firstImage.getHeight(), firstImage.getConfig());
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(firstImage, firstImageX, firstImageY, null);
+        canvas.drawBitmap(secondImage, secondImageX, secondImageY, null);
+        return result;
+    }
+
+    public void setAssetPosList (Point[] posList){
+        posList[0] = (new Point(170, 1530));
+        posList[1] = (new Point(310, 1530));
+        posList[2] = (new Point(330, 1430));
+        posList[3] = (new Point(360, 1340));
+        posList[4] = (new Point(430, 1240));
+        posList[5] = (new Point(500, 1170));
+        posList[6] = (new Point(580, 1070));
+        posList[7] = (new Point(650, 940));
+        posList[8] = (new Point(680, 840));
+        posList[9] = (new Point(700, 740));
+        posList[10] = (new Point(710, 640));
+        posList[11] = (new Point(800, 540));
+
+        /**
+         * Vägdelning Rutt A
+         */
+        posList[12] = (new Point(880, 450));
+        posList[13] = (new Point(880, 350));
+        posList[14] = (new Point(900, 250));
+        posList[15] = (new Point(970, 170));
+        posList[16] = (new Point(1100, 150));
+        posList[17] = (new Point(1220, 170));
+        posList[18] = (new Point(1320, 250));
+        posList[19] = (new Point(1440, 330));
+        posList[20] = (new Point(1520, 430));
+        posList[21] = (new Point(1520, 530));
+        posList[22] = (new Point(1500, 610));
+
+
+
+        /**
+         * Vägdelning Rutt B
+         */
+
+
+        /**
+         * Sammankoppling
+         */
+        posList[23] = (new Point(1600, 710));
+        posList[24] = (new Point(1585, 810));
+        posList[25] = (new Point(1550, 1300));
+        posList[26] = (new Point(1550, 1400));
+
+        /**
+         * Alt rutt A
+         */
+        posList[27] = (new Point(1460, 1500));
+
+        /**
+         * Alt Rutt B
+         */
+
+        /**
+         * Sammankoppling
+         */
+        posList[28] = (new Point(1330, 1550));
+        posList[29] = (new Point(1200, 1570));
+        posList[30] = (new Point(1100, 1620));
+        posList[31] = (new Point(1000, 1690));
+        posList[32] = (new Point(860, 1670));
+        posList[33] = (new Point(710, 1690));
+        posList[34] = (new Point(600, 1730));
+        posList[35] = (new Point(450, 1700));
+        posList[36] = (new Point(320, 1620));
 
     }
 
--- app/src/main/java/se/iths/apostolidis/quickmaths/MPhotoView.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/MPhotoView.java
@@ -9,6 +9,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -22,7 +24,8 @@ import com.github.chrisbanes.photoview.PhotoView;
  */
 
 public class MPhotoView extends PhotoView {
-
+    private Bitmap backgroundImage;
+    private LayerDrawable coordinateAssets;
     int gridSize = 35;
     private Point[] coordinatesY = new Point[gridSize];
     private Point[] coordinatesX = new Point[gridSize];
@@ -37,20 +40,6 @@ public class MPhotoView extends PhotoView {
         super(context);
     }
 
-    public MPhotoView (Context context, int maxX, int maxY){
-        super(context);
-        //this.coordinatesX = coordinatesX;
-        //this.coordinatesY = coordinatesY;
-        this.maxX = maxX;
-        this.maxY = maxY;
-
-    }
-    public void setCoordinatesY (Point[] posY){
-        coordinatesY = posY;
-    }
-    public void setCoordinatesX (Point[] posX){
-        coordinatesX = posX;
-    }
 
     public MPhotoView(Context context, AttributeSet attr) {
         super(context, attr);
@@ -60,181 +49,18 @@ public class MPhotoView extends PhotoView {
         super(context, attr, defStyle);
     }
 
-    public MPhotoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.d("Wille", String.valueOf(canvas.getWidth()));
-        Log.d("Wille", String.valueOf(canvas.getHeight()));
+        Log.d("Wille", "Canvas width: " + String.valueOf(canvas.getWidth()));
+        Log.d("Wille", "Canvas height: " + String.valueOf(canvas.getHeight()));
 
         canvas.save();
         super.onDraw(canvas);
         canvas.restore();
 
-        //checkPointDrawables(canvas);
-
-        drawPoint(canvas);
-        //bror(canvas);
-
-        //canvas.setMatrix(this.getAttacher().getImageMatrix());
-        //gridCoordinates(canvas);
     }
 
-    public void setAssetPosList (Point[] posList){
-        posList[0] = (new Point(170, 1530));
-        posList[1] = (new Point(310, 1530));
-        posList[2] = (new Point(330, 1430));
-        posList[3] = (new Point(360, 1340));
-        posList[4] = (new Point(430, 1240));
-        posList[5] = (new Point(500, 1170));
-        posList[6] = (new Point(580, 1070));
-        posList[7] = (new Point(650, 940));
-        posList[8] = (new Point(680, 840));
-        posList[9] = (new Point(700, 740));
-        posList[10] = (new Point(710, 640));
-        posList[11] = (new Point(800, 540));
-
-        /**
-         * Vägdelning Rutt A
-         */
-        posList[12] = (new Point(880, 450));
-        posList[13] = (new Point(880, 350));
-        posList[14] = (new Point(900, 250));
-        posList[15] = (new Point(970, 170));
-        posList[16] = (new Point(1100, 150));
-        posList[17] = (new Point(1220, 170));
-        posList[18] = (new Point(1320, 250));
-        posList[19] = (new Point(1440, 330));
-        posList[20] = (new Point(1520, 430));
-        posList[21] = (new Point(1520, 530));
-        posList[22] = (new Point(1500, 610));
-
-
-
-        /**
-         * Vägdelning Rutt B
-         */
-
-
-        /**
-         * Sammankoppling
-         */
-        posList[23] = (new Point(1600, 710));
-        posList[24] = (new Point(1585, 810));
-        posList[25] = (new Point(1550, 1300));
-        posList[26] = (new Point(1550, 1400));
-
-        /**
-         * Alt rutt A
-         */
-        posList[27] = (new Point(1460, 1500));
-
-        /**
-         * Alt Rutt B
-         */
-
-        /**
-         * Sammankoppling
-         */
-        posList[28] = (new Point(1330, 1550));
-        posList[29] = (new Point(1200, 1570));
-        posList[30] = (new Point(1100, 1620));
-        posList[31] = (new Point(1000, 1690));
-        posList[32] = (new Point(860, 1670));
-        posList[33] = (new Point(710, 1690));
-        posList[34] = (new Point(600, 1730));
-        posList[35] = (new Point(450, 1700));
-        posList[36] = (new Point(320, 1620));
-
-    }
-    public void checkPointDrawables (Canvas canvas){
-        Bitmap coordinate = BitmapFactory.decodeResource(getResources(), R.drawable.arrowright);
-        canvas.save();
-        canvas.setMatrix(this.getAttacher().getImageMatrix());
-        Bitmap b = coordinate.createScaledBitmap(coordinate, 100, 100, false);
-        canvas.drawBitmap(b, 450, 1700, null);
-        canvas.restore();
-
-        Bitmap coordinate2 = BitmapFactory.decodeResource(getResources(), R.drawable.arrowright);
-        canvas.save();
-        canvas.setMatrix(this.getAttacher().getImageMatrix());
-        Bitmap b2 = coordinate.createScaledBitmap(coordinate2, 100, 100, false);
-        canvas.drawBitmap(b2, 320, 1620, null);
-        canvas.restore();
-    }
-    public void drawPoint (Canvas canvas){
-//        paint.setStrokeWidth(30);
-//        canvas.save();
-//        canvas.drawLine(0,0, 200, 200, paint);
-//        canvas.restore();
-
-        Point[] assetPosList = new Point[37];
-        setAssetPosList(assetPosList);
-
-        for (int i = 0; i < assetPosList.length; i++) {
-
-            canvas.save();
-            canvas.setMatrix(this.getAttacher().getImageMatrix());
-            //canvas.drawRect(new Rect(coordinatesX[i].x, (coordinatesX[i].x + 20), 50, 100), new Paint(Color.BLACK));
-            canvas.drawCircle(assetPosList[i].x + 40, assetPosList[i].y + 60, 20,  new Paint(Color.BLACK));
-            canvas.restore();
-
-            int x = canvas.getWidth();
-            Log.d("Wille", "Canvas Width: " + x);
-
-            /**
-             * Image loop
-             */
-//            Bitmap coordinate = BitmapFactory.decodeResource(getResources(), R.drawable.arrowright);
-//            canvas.save();
-//            canvas.setMatrix(this.getAttacher().getImageMatrix());
-//            Bitmap b = coordinate.createScaledBitmap(coordinate, 100, 100, false);
-//            canvas.drawBitmap(b, assetPosList[i].x, assetPosList[i].y, null);
-//            canvas.restore();
-//
-//            Log.d("Wille", "Xpos: " + assetPosList[i].x);
-//            Log.d("Wille", "Ypos: " + assetPosList[i].y);
-        }
-
-    }
-
-    public void bror(Canvas canvas){
-        int incrementY;
-        int incrementX;
-        int X = canvas.getWidth() + 700;
-        int Y = canvas.getHeight();
-        int spaceX = X / gridSize;
-        int spaceY = Y / gridSize;
-        int index = 0;
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setTextSize(40);
-
-
-
-        incrementY = 0;
-        for (int j = 0; j < gridSize; j++) {
-            canvas.save();
-            canvas.drawText(String.valueOf(j), 0, 50 + incrementY, paint);
-            canvas.restore();
-            incrementY += spaceY;
-
-        }
-            incrementX = 0;
-            for (int i = 0; i < gridSize; i++) {
-                canvas.save();
-                canvas.drawText(String.valueOf(i), incrementX, 50, paint);
-                canvas.restore();
-                incrementX += spaceX;
-                index += 1;
-
-            }
-
-
-
-    }
 
     public void gridCoordinates(Canvas canvas){
         int incrementY;
