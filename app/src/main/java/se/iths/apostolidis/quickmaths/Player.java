1ecec5af431e448d3a77539774c28144f52e6aa3
WilliamLennartsson <31884043+WilliamLennartsson@users.noreply.github.com>
2017-11-29 13:18:03 +0100

    Canvas update

---
+++ app/src/main/java/se/iths/apostolidis/quickmaths/Player.java
@@ -0,0 +1,74 @@
+package se.iths.apostolidis.quickmaths;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by mrx on 2017-11-28.
+ */
+
+public class Player {
+
+    private int id;
+    private String name;
+    private int gold;
+    private int posX, posY;
+    private Bitmap avatar;
+
+    public int getCoordinateIndex() {
+        return coordinateIndex;
+    }
+
+    public void setCoordinateIndex(int coordinateIndex) {
+        this.coordinateIndex = coordinateIndex;
+    }
+
+    private int coordinateIndex;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPos(int posX, int posY) {
+        this.posY = posY;
+        this.posX = posX;
+    }
+
+    public Bitmap getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(Bitmap avatar) {
+        this.avatar = avatar;
+    }
+
+}
