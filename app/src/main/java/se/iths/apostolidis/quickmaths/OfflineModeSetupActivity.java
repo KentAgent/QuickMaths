09f44a00ae8e3a512af7cb3a06cdc9c99cc39d30
Fyrren <furst.andreas@hotmail.com>
2017-12-05 20:27:34 +0100

    Från 8 till 4 players. + styling på widgets

--- app/src/main/java/se/iths/apostolidis/quickmaths/OfflineModeSetupActivity.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/OfflineModeSetupActivity.java
@@ -1,49 +1,46 @@
 package se.iths.apostolidis.quickmaths;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.michaelmuenzer.android.scrollablennumberpicker.ScrollableNumberPicker;
+import com.michaelmuenzer.android.scrollablennumberpicker.ScrollableNumberPickerListener;
 
 public class OfflineModeSetupActivity extends AppCompatActivity {
 
     int numberOfPlayers = 0;
-    int emptyPlayers = 7;
-    int hide = 7;
 
-    EditText[] editTextsPN = new EditText[8];
+
+    EditText[] editTextsPN = new EditText[4];
     EditText etP1Name;
     EditText etP2Name;
     EditText etP3Name;
     EditText etP4Name;
-    EditText etP5Name;
-    EditText etP6Name;
-    EditText etP7Name;
-    EditText etP8Name;
 
-    TextView[] textViewsPN = new TextView[8];
+
+    TextView[] textViewsPN = new TextView[4];
     TextView tvP1Name;
     TextView tvP2Name;
     TextView tvP3Name;
     TextView tvP4Name;
-    TextView tvP5Name;
-    TextView tvP6Name;
-    TextView tvP7Name;
-    TextView tvP8Name;
-
-    String[] choosenCategories;
-    int xTrue;
+    int setUpPlayers = 0;
 
 
-
+    ScrollableNumberPicker snp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline_mode_setup);
 
-
+        //TODO skapa flera intents, en som sparar vid bak och en som sparar vid framåt knapparna
 
 
 
@@ -51,51 +48,50 @@ public class OfflineModeSetupActivity extends AppCompatActivity {
         etP2Name = findViewById(R.id.editTextNamePlayer2);
         etP3Name = findViewById(R.id.editTextNamePlayer3);
         etP4Name = findViewById(R.id.editTextNamePlayer4);
-        etP5Name = findViewById(R.id.editTextNamePlayer5);
-        etP6Name = findViewById(R.id.editTextNamePlayer6);
-        etP7Name = findViewById(R.id.editTextNamePlayer7);
-        etP8Name = findViewById(R.id.editTextNamePlayer8);
+
 
         tvP1Name = findViewById(R.id.textViewPlayer1Name);
         tvP2Name = findViewById(R.id.textViewPlayer2Name);
         tvP3Name = findViewById(R.id.textViewPlayer3Name);
         tvP4Name = findViewById(R.id.textViewPlayer4Name);
-        tvP5Name = findViewById(R.id.textViewPlayer5Name);
-        tvP6Name = findViewById(R.id.textViewPlayer6Name);
-        tvP7Name = findViewById(R.id.textViewPlayer7Name);
-        tvP8Name = findViewById(R.id.textViewPlayer8Name);
+
 
         textViewsPN[0] = tvP1Name;
         textViewsPN[1] = tvP2Name;
         textViewsPN[2] = tvP3Name;
         textViewsPN[3] = tvP4Name;
-        textViewsPN[4] = tvP5Name;
-        textViewsPN[5] = tvP6Name;
-        textViewsPN[6] = tvP7Name;
-        textViewsPN[7] = tvP8Name;
+
 
         editTextsPN[0] = etP1Name;
         editTextsPN[1] = etP2Name;
         editTextsPN[2] = etP3Name;
         editTextsPN[3] = etP4Name;
-        editTextsPN[4] = etP5Name;
-        editTextsPN[5] = etP6Name;
-        editTextsPN[6] = etP7Name;
-        editTextsPN[7] = etP8Name;
 
 
+        snp = findViewById(R.id.number_picker_horizontal);
+        int nrOfPlayers = 1;
+
+        ScrollableNumberPickerListener nrPlayersListerner = new ScrollableNumberPickerListener() {
+            @Override
+            public void onNumberPicked(int value) {
+                setUpPlayers = snp.getValue();
+
+            }
+        };
+        snp.setListener(nrPlayersListerner);
         makeAllInvisable();
+        Intent intent = new Intent();
+        //Bundle extras = getIntent().getExtras();
 
-        Bundle extras = getIntent().getExtras();
-
-        numberOfPlayers = extras.getInt("numberOfPlayers");
+        //numberOfPlayers = extras.getInt("numberOfPlayers");
         if(numberOfPlayers != 0) {
-            for (int i = 0; i < numberOfPlayers; i++) {
+            for (int i = 0; i < nrOfPlayers; i++) {
                 editTextsPN[i].setVisibility(View.VISIBLE);
                 textViewsPN[i].setVisibility(View.VISIBLE);
 
             }
         }
+
 
 
 
@@ -146,6 +142,17 @@ public class OfflineModeSetupActivity extends AppCompatActivity {
                 n.setVisibility(View.INVISIBLE);
             }
     }
+
+    public void makePlayersVisable(View view){
+        int players = snp.getValue();
+        for (int i = 0; i < setUpPlayers; i++) {
+            editTextsPN[i].setVisibility(View.VISIBLE);
+            textViewsPN[i].setVisibility(View.VISIBLE);
+
+        }
+    }
+
+
 }
 
 
