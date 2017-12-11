b30dc53dcad6b1a59ab3ac77500ae9c69f852f9e
simonwinther <simon.winther@hotmail.com>
2017-12-11 16:48:29 +0100

    rezize och move the plupps

--- app/src/main/java/se/iths/apostolidis/quickmaths/GameActivity.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/GameActivity.java
@@ -478,34 +478,34 @@ public class GameActivity extends AppCompatActivity {
     //OFFLINE
     public void setAssetPosListOffline(Point[] posList) {
 
-        posList[0] = (new Point(170, 1530));
-        posList[1] = (new Point(310, 1530));
-        posList[2] = (new Point(330, 1430));
-        posList[3] = (new Point(360, 1340));
-        posList[4] = (new Point(430, 1240));
-        posList[5] = (new Point(500, 1170));
-        posList[6] = (new Point(580, 1070));
-        posList[7] = (new Point(650, 940));
-        posList[8] = (new Point(680, 840));
-        posList[9] = (new Point(700, 740));
-        posList[10] = (new Point(710, 640));
-        posList[11] = (new Point(800, 540));
+        posList[0] = (new Point(200, 1500));
+        posList[1] = (new Point(330, 1500));
+        posList[2] = (new Point(370, 1400));
+        posList[3] = (new Point(400, 1290));
+        posList[4] = (new Point(470, 1220));
+        posList[5] = (new Point(550, 1150));
+        posList[6] = (new Point(615, 1050));
+        posList[7] = (new Point(666, 960));
+        posList[8] = (new Point(695, 810));
+        posList[9] = (new Point(720, 670));
+        posList[10] = (new Point(800, 590));
+        posList[11] = (new Point(880, 500));
 
 
               //Vägdelning Rutt A
 
 
-            posList[12] = (new Point(880, 450));
-            posList[13] = (new Point(880, 350));
-            posList[14] = (new Point(900, 250));
-            posList[15] = (new Point(970, 170));
-            posList[16] = (new Point(1100, 150));
-            posList[17] = (new Point(1220, 170));
-            posList[18] = (new Point(1320, 250));
-            posList[19] = (new Point(1440, 330));
-            posList[20] = (new Point(1520, 430));
-            posList[21] = (new Point(1520, 530));
-            posList[22] = (new Point(1500, 610));
+            posList[12] = (new Point(880, 380));
+            posList[13] = (new Point(915, 270));
+            posList[14] = (new Point(1000, 210));
+            posList[15] = (new Point(1110, 200));
+            posList[16] = (new Point(1220, 250));
+            posList[17] = (new Point(1320, 330));
+            posList[18] = (new Point(1440, 390));
+            posList[19] = (new Point(1500, 480));
+            posList[20] = (new Point(1510, 600));
+            posList[21] = (new Point(1530, 700));
+            posList[22] = (new Point(1570, 800));
 
 
 
@@ -516,15 +516,15 @@ public class GameActivity extends AppCompatActivity {
 
             // Sammankoppling
 
-            posList[23] = (new Point(1600, 710));
-            posList[24] = (new Point(1585, 810));
-            posList[25] = (new Point(1550, 1300));
-            posList[26] = (new Point(1550, 1400));
+            posList[23] = (new Point(1530, 880));
+            posList[24] = (new Point(1517, 1030));
+            posList[25] = (new Point(1510, 1170));
+            posList[26] = (new Point(1500, 1350));
 
 
              // Alt rutt A
 
-            posList[27] = (new Point(1460, 1500));
+            posList[27] = (new Point(1460, 1450));
 
 
               //Alt Rutt B
@@ -533,15 +533,15 @@ public class GameActivity extends AppCompatActivity {
 
               // Sammankoppling
 
-            posList[28] = (new Point(1330, 1550));
-            posList[29] = (new Point(1200, 1570));
-            posList[30] = (new Point(1100, 1620));
-            posList[31] = (new Point(1000, 1690));
-            posList[32] = (new Point(860, 1670));
-            posList[33] = (new Point(710, 1690));
-            posList[34] = (new Point(600, 1730));
-            posList[35] = (new Point(450, 1700));
-            posList[36] = (new Point(320, 1620));
+            posList[28] = (new Point(1330, 1500));
+            posList[29] = (new Point(1200, 1520));
+            posList[30] = (new Point(1100, 1570));
+            posList[31] = (new Point(1000, 1640));
+            posList[32] = (new Point(860, 1650));
+            posList[33] = (new Point(710, 1640));
+            posList[34] = (new Point(600, 1680));
+            posList[35] = (new Point(480, 1660));
+            posList[36] = (new Point(380, 1600));
 
         }
 
